7ab85e9694e1c511026a86071da94fd58e244528
hijaziimarwa <hijaziimarwa@gmail.com>
2025-05-31 21:38:27 +0300

    Add user input collection for name, age, GPA, field of interest, and graduation status

---
+++ profile_checker.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gpa = float(input("Enter your score (0-5): "))
+field_of_interest = input("Enter your field of interest: ")
+has_graduated = input("Are you graduated? (yes/no): ")
+
+
+
